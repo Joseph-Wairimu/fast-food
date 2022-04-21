49ca0b099340f3278b09c88aa2d7e9fc6af3ee35
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-21 20:18:13 +0300

    forms

--- food/views.py
+++ food/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile,Orders,OrderUpdate,Product
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -8,6 +9,10 @@ def index(request):
 @login_required(login_url='login') 
 def menu(request):
     return render(request,'menu.html')
+
+def services(request):
+    return render(request,'services.html')
+
 
 def register(response):
     if response.method == "POST":
@@ -21,3 +26,10 @@ def register(response):
     else:
         form = RegisterForm()
     return render(response, 'register/register.html', {'form': form})
+def profile(request):
+    return render(request,'profile.html')
+
+def order(request):
+
+
+    return render(request,'order.html')
